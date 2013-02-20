2a28f695c5d0f21f49d1b41d9cb1fcacf9f3c01b
Alin Jerpelea <jerpelea@gmail.com>
2013-02-20 17:44:44 +0200

    odin: initial push

---
+++ BoardConfig.mk
@@ -0,0 +1,39 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from the common fusion3 definitions
+-include device/sony/fusion3-common/BoardConfigCommon.mk
+
+# inherit from the proprietary version
+-include vendor/sony/odin/BoardConfigVendor.mk
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := C6503,C6506,odin
+
+TARGET_SPECIFIC_HEADER_PATH += device/sony/odin/include
+
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/odin/bluetooth
+
+TARGET_KERNEL_CONFIG := cyanogen_fusion3_odin_defconfig
+
+# Partition information
+BOARD_VOLD_MAX_PARTITIONS := 26
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+
+# Recovery
+TARGET_RECOVERY_INITRC := device/sony/odin/recovery/init.rc
---
+++ cm.mk
@@ -0,0 +1,19 @@
+$(call inherit-product, device/sony/odin/full_odin.mk)
+
+# Inherit CM common GSM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6503_1270-4066 BUILD_FINGERPRINT=Sony/C6503_1270-4066/C6503:4.1.2/10.1.A.0.350/ofp_rw:userdebug/release-keys PRIVATE_BUILD_DESC="C6503-userdebug 4.1.2 10.1.A.1.350 ofp_rw test-keys"
+
+PRODUCT_NAME := cm_odin
+PRODUCT_DEVICE := odin
