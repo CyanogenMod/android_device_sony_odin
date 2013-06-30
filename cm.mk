1f3573fc30244d3f0b3068cc58ba17509ddfb94e
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-06-30 08:51:59 +0200

    odin: update signature from 10.3.A.0.423
    Change-Id: Id6e03f99529e539ba2f878fe4b4d7a077d66da2e

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6503 BUILD_FINGERPRINT=Sony/C6503_1272-3738/C6503:4.1.2/10.1.1.A.1.253/Avt3rw:user/release-keys PRIVATE_BUILD_DESC="C6503-user 4.1.2 10.1.1.A.1.253 Avt3rw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6503 BUILD_FINGERPRINT=Sony/C6503_1271-0336/C6503:4.2.2/10.3.A.0.423/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="C6503-user 4.2.2 10.3.A.0.423 WP5_rg test-keys"
 
 PRODUCT_NAME := cm_odin
 PRODUCT_DEVICE := odin
