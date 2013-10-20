c3820972b24935a0b7d4007e70ea7cff0f2127d4
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-10-20 16:35:50 +0200

    odin: use common resources
    Change-Id: I1406f6b1d3e4ea47b8321526621ac420eba1e345

--- full_odin.mk
+++ full_odin.mk
@@ -33,7 +33,7 @@ PRODUCT_PACKAGES := \
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Common Sony Resources
-$(call inherit-product, device/sony/common/resources-xxhdpi.mk)
+$(call inherit-product, device/sony/common/resources.mk)
 
 # Inherit from odin device
 $(call inherit-product, device/sony/odin/odin.mk)
