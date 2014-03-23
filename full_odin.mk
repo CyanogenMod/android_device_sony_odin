9c4c5d5a2d6316c0e0a8e99f77bf66fb38235bed
Emil <suleymanovemil8@gmail.com>
2014-03-23 13:47:21 +0400

    IR Remote support for odin
    This is my projects github repo https://github.com/sssemil/SonyIRRemote
    
    Change-Id: Ie8eed64323a7f993a2783a781931d2ab5f143e85

--- full_odin.mk
+++ full_odin.mk
@@ -27,7 +27,9 @@ TARGET_SCREEN_WIDTH := 1080
  
 # Torch
 PRODUCT_PACKAGES := \
-    Torch
+    Torch \
+    SonyIRRemote \
+    libjni_sonyopenir
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
