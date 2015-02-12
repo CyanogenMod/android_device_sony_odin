caf275ea517180ba48c81fd144618c5f89bc7d25
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-02-12 13:19:03 +0100

    odin: remove ir blaster flag
    Change-Id: I7a0ff711f0ac3b0d040104b7a1b6b3d8bc4bdd82

--- odin.mk
+++ odin.mk
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-BOARD_HAVE_IR_BLASTER := true
-
 # Inherit the fusion-common definitions
 $(call inherit-product, device/sony/fusion3-common/fusion3.mk)
 
