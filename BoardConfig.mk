0aa8b141638a1a8b4ce385fdfdb767b5b8e875e1
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-07-11 15:26:42 +0200

    remove chargemon, add backlight node for healthd
    Change-Id: I737a049c2020436484db6d89176d935a69c36327

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,9 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/odin/bluetooth
 
 TARGET_KERNEL_CONFIG := cm_fusion3_odin_defconfig
 
+# Healthd
+BACKLIGHT_PATH := /sys/class/leds/lm3533-lcd-bl-1/brightness
+
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 26
 
